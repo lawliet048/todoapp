d9f6ff6db5d3562ae00116491e08c7c7288f1eaf
aswinak <skaswin01@gmail.com>
2025-04-12 20:41:15 +0530

    added the db url

--- server/server.js
+++ server/server.js
@@ -8,14 +8,21 @@ app.use(cors());
 app.use(express.json());
 
 
-const pool = new Pool({
+/*const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+});*/
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // If using SSL on Render (most likely yes):
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 
 // Get all tasks
